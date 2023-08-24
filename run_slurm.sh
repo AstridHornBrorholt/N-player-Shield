50cedee3e8e64b8746619e97548245f24d24b831
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-08-24 15:39:35 +0200

    going back to flying blind

--- run_slurm.sh
+++ run_slurm.sh
@@ -3,29 +3,28 @@ echo "Scheduling slurm jobs"
 export log_output="$HOME/Results/N-player CC/log.txt"
 
 date > "$log_output * Starting..."
-OUT="-o=$log_output"
-ARGS="--exclude=rome0[1-3],dhabi0[1-3],naples0[1-3],vmware0[1-4] --partition=cpu -n1 --mem=16G "
+ARGS="--out=/dev/null --exclude=rome0[1-3],dhabi0[1-3],naples0[1-3],vmware0[1-4] --partition=cpu -n1 --mem=16G "
 
 export runs=1000
 export checks=1000
 export max_cars=6
-sbatch -o="$log_output" $ARGS ./run_single.sh
+sbatch $ARGS ./run_single.sh
 echo "Job scheduled."
 
 export runs=2000
 export checks=1000
 export max_cars=6
-sbatch -o="$log_output" $ARGS ./run_single.sh
+sbatch $ARGS ./run_single.sh
 echo "Job scheduled."
 
 export runs=4000
 export checks=1000
 export max_cars=6
-sbatch -o="$log_output" $ARGS ./run_single.sh
+sbatch $ARGS ./run_single.sh
 echo "Job scheduled."
 
 export runs=8000
 export checks=1000
 export max_cars=6
-sbatch -o="$log_output" $ARGS ./run_single.sh
+sbatch $ARGS ./run_single.sh
 echo "Job scheduled."
