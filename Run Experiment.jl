9589f979f07c6f8432510fadb53f21a70ac5818b
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-08-24 14:32:29 +0200

    Enough bash; do anything complicated in Julia

---
+++ Run Experiment.jl
@@ -0,0 +1,62 @@
+using Pkg
+Pkg.activate(".")
+
+using ArgParse
+include("Create Fleet.jl")
+
+⨝ = joinpath
+
+begin
+	s = ArgParseSettings()
+	@add_arg_table s begin
+	     "--runs"
+			arg_type=Int
+			required=true
+		"--checks"
+			arg_type=Int
+			required=true
+		"--max-cars"
+			arg_type=Int
+			required=true
+		"--results-dir"
+			default=homedir() ⨝ "Results/N-player CC"
+        "--verifyta-path"
+            default=homedir() ⨝ "opt/uppaal-5.0.0-linux64/bin/verifyta.sh"
+        "--blueprint-path"  
+            default=pwd() ⨝ "Fleet_blueprint.xml"
+        "--shield-path"  
+            default=pwd() ⨝ "Shield/libshield.so"
+	end
+end;
+
+args = parse_args(s)
+
+runs = args["runs"]
+checks = args["checks"]
+max_cars = args["max-cars"]
+results_dir = args["results-dir"]
+verifyta_path = args["verifyta-path"]
+
+verifyta_args = "-s --epsilon 0.001 --max-iterations 1 --good-runs $runs --total-runs $runs --runs-pr-state $runs"
+
+blueprint_path = args["blueprint-path"]
+shield_path = args["shield-path"]
+
+verifyta_call = String[
+    verifyta_path,
+    split(verifyta_args, " ")...
+]
+
+isdir(results_dir) || mkdir(results_dir)
+isdir(results_dir ⨝ "Query Results") || mkdir(results_dir ⨝ "Query Results")
+
+strategy_paths = []
+for N in 2:max_cars
+    @info "Running Fleet of $N Cars..."
+    outfile = results_dir ⨝ "Query Results/Fleet of $N Cars.txt"
+    model_path, queries_path = create_fleet(blueprint_path, strategy_paths, shield_path, results_dir; checks)
+    open(outfile, "w") do io
+        result = [verifyta_call..., model_path, queries_path] |> Cmd |> read |> String
+        write(io, result)
+    end
+end
--- run_single.sh
+++ run_single.sh
@@ -2,39 +2,6 @@
 
 ## Parameters ##
 
-julia=${julia:-"/nfs/home/cs.aau.dk/oq82yk/julia-1.9.2/bin/julia"}
-verifyta=${verifyta:-"/nfs/home/cs.aau.dk/oq82yk/uppaal-5.0.0-rc2-linux64/bin/verifyta.sh"}
-results="$HOME/Results/N-player CC"
-mkdir -p "$HOME/Results"
-mkdir -p "$results"
-mkdir -p "$results/Query Results"
-shield="$results/libshield.so"
-cp "$(pwd)/Shield/libshield.so" "$shield"
-blueprint="$(pwd)/Fleet_blueprint.xml"
-strategies=()
+julia=${julia:-"$HOME/julia-1.9.2/bin/julia"}
 
-verifyta_args="-s
-        --epsilon 0.001
-        --max-iterations 1
-        --good-runs $runs
-        --total-runs $runs
-        --runs-pr-state $runs"
-
-for ((N=2; N<=$max_cars; N++))
-do
-        echo "ⓘ $(date +%T) Running Fleet of ${N} Cars..."
-        outfile="$results/Query Results/Fleet of $N Cars.txt"
-        model_and_query=$($julia 'Create Fleet.jl' \
-                --blueprint-path "$blueprint" \
-                --strategy-paths "${strategies[@]}" \
-                --shield-path "$shield" \
-                --destination "$results" \
-                --checks $checks )
-
-        echo $model_and_query > "$log_output"
-
-        # https://stackoverflow.com/a/21163341/10595676
-        # eval to avoid model_and_query being interpretred as one single argument.
-        $verifyta $verifyta_args $model_and_query > "$outfile"
-        strategies+=("$results/Models/car$((N-1)).json")
-done
+$julia "Run Experiment.jl" --runs $runs --checks $checks --max-cars $max_cars
