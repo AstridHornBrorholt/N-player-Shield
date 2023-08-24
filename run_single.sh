24792e47e20725c09107cabaf51ed6cce80dea84
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-08-24 13:45:46 +0200

    development loop that involves pushing changes

--- run_single.sh
+++ run_single.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 ## Parameters ##
+
 julia=${julia:-"/nfs/home/cs.aau.dk/oq82yk/julia-1.9.2/bin/julia"}
+verifyta=${verifyta:-"/nfs/home/cs.aau.dk/oq82yk/uppaal-5.0.0-rc2-linux64/bin/verifyta.sh"}
 results="$HOME/Results/N-player CC"
 mkdir -p "$HOME/Results"
 mkdir -p "$results"
 mkdir -p "$results/Query Results"
 shield="$results/libshield.so"
-cp './Shield/libshield.so' "$shield"
+cp "$(pwd)/Shield/libshield.so" "$shield"
 blueprint="$(pwd)/Fleet_blueprint.xml"
 strategies=()
 
@@ -29,7 +31,7 @@ do
                 --destination "$results" \
                 --checks $checks )
 
-        echo $model_and_query > $log_output
+        echo $model_and_query > "$log_output"
 
         # https://stackoverflow.com/a/21163341/10595676
         # eval to avoid model_and_query being interpretred as one single argument.
