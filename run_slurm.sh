b50071d46b03727269ba6a3b822d753c44d64915
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-08-25 09:37:58 +0200

    Try some larger numbers also

--- run_slurm.sh
+++ run_slurm.sh
@@ -5,25 +5,25 @@ export log_output="$HOME/Results/N-player CC/log.txt"
 date > "$log_output"
 ARGS="--out=/dev/null --exclude=rome0[1-3],dhabi0[1-3],naples0[1-3],vmware0[1-4] --partition=cpu -n1 --mem=16G "
 
-export runs=1000
+export runs=10000
 export checks=1000
 export max_cars=6
 sbatch $ARGS ./run_single.sh
 echo "Job scheduled."
 
-export runs=2000
+export runs=20000
 export checks=1000
 export max_cars=6
 sbatch $ARGS ./run_single.sh
 echo "Job scheduled."
 
-export runs=4000
+export runs=40000
 export checks=1000
 export max_cars=6
 sbatch $ARGS ./run_single.sh
 echo "Job scheduled."
 
-export runs=8000
+export runs=80000
 export checks=1000
 export max_cars=6
 sbatch $ARGS ./run_single.sh
