1c623ddfbcad270cfc4688ae0dded441ad4c70d7
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-08-30 12:50:24 +0200

    Ah right run_slurm should export repetitions var

--- run_slurm.sh
+++ run_slurm.sh
@@ -5,26 +5,25 @@ export log_output="$HOME/Results/N-player CC/log.txt"
 date >> "$log_output"
 ARGS="--out=/dev/null --exclude=rome0[1-3],dhabi0[1-3],naples0[1-3],vmware0[1-4] --partition=cpu -n1 --mem=16G "
 
-export runs=10000
-export checks=1000
 export max_cars=6
+export repetitions=3
+
+export runs=100
+export checks=10
 sbatch $ARGS ./run_single.sh
 echo "Job scheduled."
 
-export runs=20000
-export checks=1000
-export max_cars=6
+export runs=200
+export checks=10
 sbatch $ARGS ./run_single.sh
 echo "Job scheduled."
 
-export runs=40000
-export checks=1000
-export max_cars=6
+export runs=400
+export checks=10
 sbatch $ARGS ./run_single.sh
 echo "Job scheduled."
 
-export runs=80000
-export checks=1000
-export max_cars=6
+export runs=800
+export checks=10
 sbatch $ARGS ./run_single.sh
 echo "Job scheduled."
