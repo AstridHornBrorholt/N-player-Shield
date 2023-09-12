afca3ed31843cd165f47094478bf4813bcf1437c
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-09-12 09:53:48 +0200

    zip centralized results also

--- zip_results.sh
+++ zip_results.sh
@@ -1,5 +1,7 @@
 $previous_working_dir=$(pwd)
 cd ~/Results
 rm "N-player CC.zip"
+rm "N-player CC Centralized.zip"
 zip -r "N-player CC.zip" "N-player CC"/*
+zip -r "N-player CC Centralized.zip" "N-player CC Centralized"/*
 cd $previous_working_dir
