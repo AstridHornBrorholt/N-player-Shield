e11b06122c95892cd94d3f2eef06422dc99ace58
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-09-18 14:02:27 +0200

    Ready for a run with the non-specialized controllers

--- run_slurm.sh
+++ run_slurm.sh
@@ -12,23 +12,8 @@ repetitions=10
 for ((r=1; r<=$repetitions; r++))
 do
     export repetition=$r
-
-    export runs=2500
-    export checks=1000
-    sbatch $ARGS ./run_single.sh
-    echo "Job scheduled."
-
-    export runs=5000
-    export checks=1000
-    sbatch $ARGS ./run_single.sh
-    echo "Job scheduled."
-
-    export runs=10000
-    export checks=1000
-    sbatch $ARGS ./run_single.sh
-    echo "Job scheduled."
-
-    export runs=20000
+    
+    export runs=20001
     export checks=1000
     sbatch $ARGS ./run_single.sh
     echo "Job scheduled."
