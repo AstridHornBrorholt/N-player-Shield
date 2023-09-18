61acbc0d3671354da27ccf6d8cc0d5776e2fd184
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-09-18 13:51:05 +0200

    Only 20k for the centralized controller, but 10 repetitions

--- Centralized Controller/run_slurm.sh
+++ Centralized Controller/run_slurm.sh
@@ -7,26 +7,11 @@ date >> "$log_output"
 ARGS="--out=/dev/null --exclude=rome0[1-3],dhabi0[1-3],naples0[1-3],vmware0[1-4] --partition=cpu -n1 --mem=4G --job-name CentCC"
 
 export max_cars=10
-repetitions=1
+repetitions=10
 
 for ((r=1; r<=$repetitions; r++))
 do
     export repetition=$r
-
-    export runs=2500
-    export checks=1000
-    sbatch $ARGS ./run_single.sh
-    echo "Job scheduled."
-
-    export runs=5000
-    export checks=1000
-    sbatch $ARGS ./run_single.sh
-    echo "Job scheduled."
-
-    export runs=10000
-    export checks=1000
-    sbatch $ARGS ./run_single.sh
-    echo "Job scheduled."
 
     export runs=20000
     export checks=1000
