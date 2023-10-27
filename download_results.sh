7119c91ed6e0ecefb453c096a111912f62257973
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-10-27 09:54:03 +0200

    Support for downloading results

--- download_results.sh
+++ download_results.sh
@@ -1,9 +1,10 @@
 scp -rd 'deismcc:/nfs/home/cs.aau.dk/oq82yk/Results/N-player\ CC.zip' "$(pwd)" > /dev/null
-scp -rd 'deismcc:/nfs/home/cs.aau.dk/oq82yk/Results/N-player\ CC\ Centralized.zip' "$(pwd)" > /dev/null
+scp -rd 'deismcc:/nfs/home/cs.aau.dk/oq82yk/Results/N-player\ CC\ Centralized\ Controller.zip' "$(pwd)" > /dev/null
+scp -rd 'deismcc:/nfs/home/cs.aau.dk/oq82yk/Results/N-player\ CC\ Centralized\ Shield.zip' "$(pwd)" > /dev/null
 unzip -qo "./N-player CC.zip" -d "$HOME/Results/"
-unzip -qo "./N-player CC Centralized.zip" -d "$HOME/Results/"
+unzip -qo "./N-player CC Centralized Controller.zip" -d "$HOME/Results/"
+unzip -qo "./N-player CC Centralized Shield.zip" -d "$HOME/Results/"
 rm "./N-player CC.zip"
-rm "./N-player CC Centralized.zip"
-tree "$HOME/Results/N-player CC"
-tree "$HOME/Results/N-player CC Centralized"
-sed -i "s>/nfs/home/cs.aau.dk/.*yk>/home/asger>g" "$HOME/Results/N-player CC*/*Runs/Repetition*/Models/*"
+rm "./N-player CC Centralized Controller.zip"
+rm "./N-player CC Centralized Shield.zip"
+sed -i "s>/nfs/home/cs.aau.dk/.*yk>/home/asger>g" $HOME/Results/N-player*/*Runs/Repetition*/Models/*
