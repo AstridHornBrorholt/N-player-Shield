fa2e34bc337a9e8fbdda05d7b73f8e02bcb85720
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2023-12-21 15:06:12 +0100

    Let's just see how this goes

--- CC/run_slurm.sh
+++ CC/run_slurm.sh
@@ -6,14 +6,14 @@ export log_output="$HOME/Results/N-player CC/log.txt"
 date >> "$log_output"
 ARGS="--out=/dev/null --exclude=rome0[1-3],dhabi0[1-3],naples0[1-3],vmware0[1-4] --partition=cpu -n1 --mem=4G --job-name StdCC"
 
-export max_cars=10
+export max_cars=50
 repetitions=10
 
 for ((r=1; r<=$repetitions; r++))
 do
     export repetition=$r
     
-    export runs=20001
+    export runs=20023
     export checks=1000
     sbatch $ARGS ./run_single.sh
     echo "Job scheduled."
