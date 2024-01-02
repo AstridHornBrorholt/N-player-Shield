7a07152b6375daab52f2c7cc7334052be1283b1e
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-01-02 14:55:23 +0100

    Copy-pasta for CP/Run Experiment.jl

---
+++ CP/Run Experiment.jl
@@ -0,0 +1,106 @@
+## Preface ##
+using Dates
+# The fruit is there to distinguish different runs writing to the same output concurrently. This doesn't seem to be a problem after all but I enjoy the splash of colour.
+emoji = rand(split("🍇🍈🍉🍊🍋🍌🍍🥭🍎🍏🍐🍑🍒🍓🫐🥝🍅🫒🥥", ""))
+🍎 = join(rand(emoji, 2), "")
+function status(str) 
+    time = Dates.format(Dates.now(), "dd/mm HH:MM")
+    println("$time $🍎 $str")
+    flush(stdout)
+end
+using Pkg
+Pkg.activate("..")
+
+using ArgParse
+include("Create Fleet.jl")
+
+## Args and Constants ##
+⨝ = joinpath
+← = push!
+
+begin
+	s = ArgParseSettings()
+	@add_arg_table s begin
+	     "--runs"
+			arg_type=Int
+			required=true
+		"--checks"
+			arg_type=Int
+			required=true
+        "--max-cars"
+            arg_type=Int
+            required=true
+        "--repetition"
+            arg_type=Int
+            required=true
+		"--results-dir"
+			default=homedir() ⨝ "Results/N-player CC"
+        "--verifyta-path"
+            default=homedir() ⨝ "opt/uppaal-5.0.0-linux64/bin/verifyta.sh"
+        "--blueprint-path"  
+            default=pwd() ⨝ "Fleet_blueprint.xml"
+        "--shield-path"  
+            default=pwd() ⨝ "../CC Shield/libshield.so"
+        "--skip-training"
+            action=:store_true
+            help="MISNOMER: Use car1.json for all cars in the fleets (must exist) without training any strategies."
+	end
+end;
+
+args = parse_args(s)
+
+runs = args["runs"]
+checks = args["checks"]
+max_cars = args["max-cars"]
+repetition = args["repetition"]
+results_dir = args["results-dir"]
+verifyta_path = args["verifyta-path"]
+skip_training = args["skip-training"]
+status("Starting... (runs=$runs, max_cars=$max_cars, repetition=$repetition, skip_training=$skip_training)")
+
+isdir(results_dir) || mkdir(results_dir) # Error if path is invalid except if it is only the last folder missing.
+isfile(verifyta_path) || error("File verifyta not found at path $verifyta_path")
+
+verifyta_args = "-s --epsilon 0.001 --max-iterations 1 --good-runs $runs --total-runs $runs --runs-pr-state $runs"
+
+verifyta_call = String[
+    verifyta_path,
+    split(verifyta_args, " ")...
+]
+
+blueprint_path = args["blueprint-path"]
+shield_path = args["shield-path"]
+
+## Resolving Paths ##
+isfile(shield_path) || error("Shield file not found at $shield_path")
+
+shield_path′ = results_dir ⨝ basename(shield_path)
+cp(shield_path, shield_path′, force=true)
+shield_path = shield_path′
+
+working_dir = results_dir ⨝ "$runs Runs" ⨝ "Repetition $repetition"
+mkpath(working_dir)
+query_results_dir = working_dir ⨝ "Query Results"
+mkpath(query_results_dir)
+models_dir = working_dir ⨝ "Models"
+mkpath(models_dir)
+
+## Mainmatter ##
+strategy_paths = String[]
+for N in 2:max_cars
+    status("Running Fleet of $N Cars...  (repetition=$repetition)")
+    outfile = query_results_dir ⨝ "Fleet of $N Cars.txt"
+    model_path, queries_path = create_fleet(blueprint_path, strategy_paths, shield_path, models_dir; checks, skip_training)
+    if skip_training
+        strategy_paths ← (working_dir ⨝ "Models/car1.json")
+    else
+        strategy_paths ← (working_dir ⨝ "Models/car$(N - 1).json")
+    end
+    open(outfile, "w") do io
+        result = [verifyta_call..., model_path, queries_path] |> Cmd |> read |> String
+        write(io, result)
+    end
+    status("Done running Fleet of $N Cars.  (repetition=$repetition)")
+end
+
+status("All done.")
