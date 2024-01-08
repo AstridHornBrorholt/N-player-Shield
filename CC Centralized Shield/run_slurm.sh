09d4401285205428c1e0176a9e9eb3f13263be78
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-01-08 11:33:17 +0100

    Ah, hell.

--- CC Centralized Shield/run_slurm.sh
+++ CC Centralized Shield/run_slurm.sh
@@ -69,28 +69,28 @@ do
     # echo "Job scheduled."
 
     export blueprint_path="$(pwd)/3-Car Declared Action_blueprint.xml"
-    export shield_path="$(pwd)/2-car-declared-action.so"
+    export shield_path="$(pwd)/2-car.so"
     export runs=2500
     export checks=1000
     sbatch $ARGS ./run_single.sh
     echo "Job scheduled."
 
     export blueprint_path="$(pwd)/3-Car Declared Action_blueprint.xml"
-    export shield_path="$(pwd)/2-car-declared-action.so"
+    export shield_path="$(pwd)/2-car.so"
     export runs=5000
     export checks=1000
     sbatch $ARGS ./run_single.sh
     echo "Job scheduled."
     
     export blueprint_path="$(pwd)/3-Car Declared Action_blueprint.xml"
-    export shield_path="$(pwd)/2-car-declared-action.so"
+    export shield_path="$(pwd)/2-car.so"
     export runs=10000
     export checks=1000
     sbatch $ARGS ./run_single.sh
     echo "Job scheduled."
     
     export blueprint_path="$(pwd)/3-Car Declared Action_blueprint.xml"
-    export shield_path="$(pwd)/2-car-declared-action.so"
+    export shield_path="$(pwd)/2-car.so"
     export runs=20000
     export checks=1000
     sbatch $ARGS ./run_single.sh
