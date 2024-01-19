5808a92a2d2e654bfd356aa574b093fb058706e9
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-01-19 14:13:26 +0100

    More sophisticated shield transfer thingy

--- CC Shield/CC Shield Transfer.jl
+++ CC Shield/CC Shield Transfer.jl
@@ -34,6 +34,24 @@ begin
 	@revise using GridShielding
 end
 
+# ╔═╡ da8a843d-b5c7-4155-b90c-3df160996c13
+md"""
+# Import a shield
+"""
+
+# ╔═╡ af869f6a-d132-4c75-b0b4-440f76352c70
+@bind imported_shield_fp FilePicker()
+
+# ╔═╡ 75dbc088-74a1-4a7a-ae59-f2163b99b6f1
+shield = if isnothing(imported_shield_fp)
+	nothing
+else
+	imported_shield_fp["data"] |> IOBuffer |> robust_grid_deserialization
+end
+
+# ╔═╡ ea83ab65-934a-4734-b562-f9f5223fb34f
+granularity = shield.granularity
+
 # ╔═╡ 1e159603-fc61-45f8-9595-f75e55318344
 md"""
 # Default Mechanics the Shield was Created With
@@ -81,9 +99,6 @@ end
 # Default Mechanics
 m = CCMechanics(1, 0, 200, 2, -10, 20, -10, 20)
 
-# ╔═╡ 6134ef59-6377-466b-952d-bee90e421b80
-s0 = (0, 0, 10)
-
 # ╔═╡ f8a8834e-b8fe-4dc8-8528-4b72148fda6f
 function random_front_behaviour(mechanics::CCMechanics, point, random_variable)
     v_ego, v_front, distance = point
@@ -97,10 +112,18 @@ function random_front_behaviour(mechanics::CCMechanics, point, random_variable)
 end
 
 # ╔═╡ 0fba5442-bba5-4a82-9821-77068368227e
-function speed_limit(min, max, v, action::CCAction)
-	if action == backwards && v <= min
+function speed_limit(mechanics::CCMechanics, agent, v, action::CCAction)
+	if agent == :ego
+		min, max = mechanics.v_ego_min, mechanics.v_ego_max
+	elseif agent == :front
+		min, max = mechanics.v_front_min, mechanics.v_front_max
+	else
+		error("Argument `agent` must be :ego or :front but was $agent")
+	end
+	acceleration = mechanics.acceleration*mechanics.t_act
+	if action == backwards && v - acceleration < min
 		return neutral
-	elseif action == forwards && v >= max
+	elseif action == forwards && v + acceleration > max
 		return neutral
 	else
 		return action
@@ -108,13 +131,13 @@ function speed_limit(min, max, v, action::CCAction)
 end
 
 # ╔═╡ 1431a6cc-1a91-4357-b624-8ed77311a426
-function apply_action(velocity, action::CCAction, acceleration=2)
+function apply_action(mechanics::CCMechanics, velocity, action::CCAction)
 	if action == backwards
-		return velocity - acceleration
+		return velocity - mechanics.acceleration*mechanics.t_act
 	elseif action == neutral
 		return velocity
 	else
-		return velocity + acceleration
+		return velocity + mechanics.acceleration*mechanics.t_act
 	end
 end
 
@@ -138,23 +161,17 @@ begin
 	
 	    front_action = random_front_behaviour(mechanics, point, random_variable)
 
-		front_action′ = speed_limit(mechanics.v_front_min, 
-			mechanics.v_front_max, 
-			v_front,
-			front_action)
+		front_action′ = speed_limit(mechanics, :front, v_front, front_action)
 		
-		v_front = apply_action(v_front, front_action′, mechanics.acceleration)
+		v_front = apply_action(mechanics, v_front, front_action′)
 	
-	    action′ = speed_limit(mechanics.v_ego_min, 
-	        mechanics.v_ego_max, 
-	        v_ego,
-	        action)
+	    action′ = speed_limit(mechanics, :ego, v_ego, action)
 	    
-	    v_ego = apply_action(v_ego, action′, mechanics.acceleration)
+	    v_ego = apply_action(mechanics, v_ego, action′)
 	
 	    new_vel = v_front - v_ego;
 	
-	    distance += (old_vel + new_vel)/2;
+	    distance += mechanics.t_act*((old_vel + new_vel)/2);
 	    (v_ego, v_front, distance)
 	end
 end
@@ -287,24 +304,6 @@ model = SimulationModel(simulation_function, randomness_space, samples_per_axis,
 # ╔═╡ bd4395db-3c26-4235-9a42-fa6a7eca7041
 reachability_function = get_barbaric_reachability_function(model)
 
-# ╔═╡ da8a843d-b5c7-4155-b90c-3df160996c13
-md"""
-## Import a shield
-"""
-
-# ╔═╡ af869f6a-d132-4c75-b0b4-440f76352c70
-@bind imported_shield_fp FilePicker()
-
-# ╔═╡ 75dbc088-74a1-4a7a-ae59-f2163b99b6f1
-shield = if isnothing(imported_shield_fp)
-	nothing
-else
-	imported_shield_fp["data"] |> IOBuffer |> robust_grid_deserialization
-end
-
-# ╔═╡ ea83ab65-934a-4734-b562-f9f5223fb34f
-granularity = shield.granularity
-
 # ╔═╡ 2418bf90-b0ec-4cb9-b3fd-bf2b91d2ff33
 md"""
 ### Inspect imported shield
@@ -312,6 +311,7 @@ md"""
 
 # ╔═╡ 107b960f-1a75-41f9-9cb9-195877ad6184
 shielded_random = s -> begin
+	s = Tuple(round(x) for x in s)
 	if s ∈ shield
 		partition = box(shield, s)
 		allowed = int_to_actions(CCAction, get_value(partition))
@@ -324,6 +324,58 @@ shielded_random = s -> begin
 	end
 end
 
+# ╔═╡ fb88f6f9-cfba-4ccd-ba56-9e84a56ce63c
+md"""
+# Modifiying the mechanics
+
+Okok so this might look a bit strange. I will admit a degree of trial and error has been employed here.
+
+But what I found was, that you cannot just change the mechanics to just about anything, if you want the shield to still be applicable. 
+
+The velocity can have 15 values: From -10 to 20 in increments of 2. If the acceleration (i.e. the increment) is changed, the min and max velocities have to be changed in a proportional manner so that there are still 15 possible velocities. 
+"""
+
+# ╔═╡ 0518e3ff-8c3f-49b4-92ba-871b22c7adc7
+collect(m.v_ego_min:m.acceleration:m.v_ego_max)
+
+# ╔═╡ 6ed54088-757e-4906-ab95-769563050ef9
+md"""
+Same has to hold true for distance. There are a lot more increments there, but the same principle applies. If the acceleration is shifted, the distance has to be shifted in a similar manner for the safety strategy to apply.
+
+And then it doesn't matter if the distance is between 0 and 200, or if it's between 20 and 220. It is relative anyway. The same goes for velocity.
+
+And lastly, something similar happens with the time-step. If the time-step is halved, in essence this is the same as halving the acceleration and velocity or something.
+"""
+
+# ╔═╡ de7136f9-32ed-4042-b649-1bdfb9624685
+begin
+	t_act_multiplier = 0.8
+	acceleration_multiplier = 0.5
+	distance_add = 25
+	velocity_add = 5
+end
+
+# ╔═╡ 3af3824c-c032-46b6-b6ae-db9f01691183
+m′ =  CCMechanics(m.t_act*t_act_multiplier,
+			(m.distance_min + distance_add)*t_act_multiplier*acceleration_multiplier,
+			(m.distance_max + distance_add)*t_act_multiplier*acceleration_multiplier,
+			m.acceleration*acceleration_multiplier,
+			(m.v_ego_min + velocity_add)*t_act_multiplier*acceleration_multiplier,
+			(m.v_ego_max + velocity_add)*t_act_multiplier*acceleration_multiplier,
+			(m.v_front_min + velocity_add)*t_act_multiplier*acceleration_multiplier,
+			(m.v_front_max + velocity_add)*t_act_multiplier*acceleration_multiplier)
+
+# ╔═╡ 4c6fac5d-86b9-47d0-a654-77e3adc679f4
+@assert length(m.v_ego_min:m.acceleration*m.t_act:m.v_ego_max) ==
+	length(m′.v_ego_min:m′.acceleration*m′.t_act:m′.v_ego_max)
+
+# ╔═╡ a9b3ccc3-58f6-4b65-9923-b4c213df6752
+@assert length(m.distance_min:m.acceleration*m.t_act/2:m.distance_max) ==
+	length(m′.distance_min:m′.acceleration*m′.t_act/2:m′.distance_max)
+
+# ╔═╡ fb24a4e7-7754-4a35-bbfe-99dcf59ba50b
+s0 = (0, 0, 10)
+
 # ╔═╡ 0382588a-ac96-4528-9fee-67ab93d4a1f8
 let
 	animation = @animate for i in 1:10
@@ -335,9 +387,18 @@ let
 	gif(animation, fps=1, show_msg=false)
 end
 
+# ╔═╡ baa9e7b6-9e93-4fe2-bf53-4c37803f3820
+simulate_sequence(m, 120, s0, shielded_random)
+
 # ╔═╡ c971bbe4-bc6b-49dd-940d-3277017e99bc
-function evaluate(m::CCMechanics, policy; episode_length=120, traces=1000, is_safe=is_safe)
+function evaluate(m::CCMechanics, policy; 
+		episode_length=120, 
+		traces=1000, 
+		is_safe=is_safe,
+		s0=s0)
+	
 	example_of_unsafe_trace = nothing
+	trace = nothing
 	safety_violations = 0
 	for i in 1:traces
 		trace = simulate_sequence(m, episode_length, s0, policy)
@@ -347,142 +408,159 @@ function evaluate(m::CCMechanics, policy; episode_length=120, traces=1000, is_sa
 			safety_violations += 1
 		end
 	end
-	return (;traces, safety_violations, example_of_unsafe_trace)
+	example_trace = something(example_of_unsafe_trace, trace)
+	return (;traces, safety_violations, example_trace)
 end
 
 # ╔═╡ 93741008-85f5-479c-908e-27a3716ef25f
-traces, safety_violations, example_of_unsafe_trace = evaluate(m, shielded_random)
+traces, safety_violations, example_trace = evaluate(m, shielded_random)
 
 # ╔═╡ db29fb3a-0e95-44f6-b324-5238ac02427c
-if !isnothing(example_of_unsafe_trace)
-	plot_sequence(example_of_unsafe_trace..., title="Example of Unsafe Trace", legend=:topleft)
-end
+plot_sequence(example_trace..., title="Example Trace", legend=:topleft)
 
 # ╔═╡ b4eea529-3c88-4e9d-b1b1-99fe2f9c4f94
-if safety_violations > 0 let
-	first_unsafe = nothing
-	states, times = example_of_unsafe_trace
-	for (i, s) in enumerate(states)
-		if !is_safe(s)
-			first_unsafe = i
-			break
+let
+	if safety_violations > 0 
+		first_unsafe = nothing
+		states, times = example_trace
+		for (i, s) in enumerate(states)
+			if !is_safe(s, m=m)
+				first_unsafe = i
+				break
+			end
 		end
-	end
-
-	if first_unsafe == 1
-		Markdown.parse("""
-		!!! danger "Initial state unsafe"
-			Initial state: $(states[first_unsafe])
-		""")
+	
+		if first_unsafe == 1
+			Markdown.parse("""
+			!!! danger "Initial state unsafe"
+				Initial state: $(states[first_unsafe])
+			""")
+		else
+			Markdown.parse("""
+			!!! danger "Unsafe trace found"
+				Shielded random agent was found to be unsafe.
+		
+				Out of $traces traces, $safety_violations were found to be unsafe.
+		
+				First unsafe sate reached: 
+				
+					$(states[first_unsafe]) at index $first_unsafe, time  $(times[first_unsafe])
+				
+				The state before that: 
+				
+					$(states[first_unsafe - 1])
+			""")
+		end
 	else
 		Markdown.parse("""
-		!!! danger "Unsafe trace found"
-			Shielded random agent was found to be unsafe.
-	
-			Out of $traces traces, $safety_violations were found to be unsafe.
-	
-			First unsafe sate reached: 
-			
-				$(states[first_unsafe])
-			
-			The state before that: 
-			
-				$(states[first_unsafe - 1])
+		!!! success "✅ All traces safe"
+			$safety_violations out of the $traces traces were unsafe.
 		""")
 	end
-end end
+end
 
-# ╔═╡ fb88f6f9-cfba-4ccd-ba56-9e84a56ce63c
-md"""
-# Modifiying the mechanics
-
-The new mechanics are like this
-"""
-
-# ╔═╡ 3af3824c-c032-46b6-b6ae-db9f01691183
-m′ = CCMechanics(1, 0, 200, 1, -5, 10, -5, 10)
-
-# ╔═╡ 2b1c93bc-d6f1-4591-8fde-c32a78138325
-s0
+# ╔═╡ 6134ef59-6377-466b-952d-bee90e421b80
+# s0′ = π(s0)
+s0′ = (0, 0, m′.distance_min + m′.distance_min/2)
 
 # ╔═╡ 38d987a3-eeb8-45fc-a86f-53e5337663c2
 begin
-	# Projection of the state space of the modified mechanics to the original mechanics
+	# Projection of the state space of the modified mechanics
+	# to that of the original mechanics
 	function π(s)
 		v_ego, v_front, distance = s
-		return (v_ego*2, v_front*2, distance*2)
+		return (v_ego/t_act_multiplier/acceleration_multiplier - velocity_add,
+				v_front/t_act_multiplier/acceleration_multiplier - velocity_add,
+				distance/t_act_multiplier/acceleration_multiplier - distance_add)
 	end
 
 	# Inverse of the projection
 	function π⁻¹(s)
 		v_ego, v_front, distance = s
-		return (v_ego*0.5, v_front*0.5, distance*0.5)
+		return ((v_ego + velocity_add)*t_act_multiplier*acceleration_multiplier,
+				(v_front + velocity_add)*t_act_multiplier*acceleration_multiplier,
+				(distance + distance_add)*t_act_multiplier*acceleration_multiplier)
 	end
 
-	let s = (10, 10, 10)
-		@assert s == π⁻¹(π(s))
+	let s = (-10, 10, 10)
+		@assert all(s .≈ π⁻¹(π(s))) "$s ≈ $(π⁻¹(π(s)))"
 	end
 end
+
+# ╔═╡ 7854e7a3-0749-45b9-85b9-ab43bcca10d1
+π⁻¹((m.v_ego_min, m.v_front_min, m.distance_min))
+
+# ╔═╡ 223fc1d3-6b1c-4807-bc74-7bf4a63ed859
+π((m′.v_ego_max, m′.v_front_max, m′.distance_max))
 
 # ╔═╡ 9d64b733-6a09-4997-b363-04281f52fb26
 shielded_random′ = s -> shielded_random(π(s))
 
-# ╔═╡ 38e1e427-6b1f-402c-93d5-8847e2563c32
-md"""
-## Applying the projection or something
-"""
-
 # ╔═╡ aea60129-59db-4597-9bb5-715b83560dd0
 # Should be safety violations here :-)
-traces′, safety_violations′, example_of_unsafe_trace′ = 
-	evaluate(m′, shielded_random′, is_safe=s -> is_safe(s, m=m′))
+traces′, safety_violations′, example_trace′ = 
+	evaluate(m′, shielded_random′, is_safe=s -> is_safe(s, m=m′), s0=s0′)
+
+# ╔═╡ 71155dd3-0abc-4119-9e31-885663a32cec
+[π(s) for s in example_trace′.states]
 
 # ╔═╡ ddd02244-044f-480c-833a-bd895f0adb79
 # But this should be unsafe.
 # Because shielding m′ with the original shield leads to safety violations.
 evaluate(m′, shielded_random)
 
+# ╔═╡ ed2811a6-c557-4f08-855c-59425a0d5c72
+max([x[1] for x in example_trace′.states]...)
+
 # ╔═╡ 0dd174f4-ca16-4ad3-90e7-783da9f59366
-if !isnothing(example_of_unsafe_trace′)
-	plot_sequence(example_of_unsafe_trace′..., title="Example of Unsafe Trace", legend=:topleft, is_safe=s -> is_safe(s, m=m′))
-end
+plot_sequence(example_trace′..., title="Example Trace", legend=:topleft, is_safe=s -> is_safe(s, m=m′))
 
 # ╔═╡ 5aaa1518-652c-4980-9899-0d7ae60008ea
-if safety_violations′ > 0 let
-	first_unsafe = nothing
-	states, times = example_of_unsafe_trace′
-	for (i, s) in enumerate(states)
-		if !is_safe(s, m=m′)
-			first_unsafe = i
-			break
+let
+	if safety_violations′ > 0 
+		first_unsafe = nothing
+		states, times = example_trace′
+		for (i, s) in enumerate(states)
+			if !is_safe(s, m=m′)
+				first_unsafe = i
+				break
+			end
 		end
-	end
-
-	if first_unsafe == 1
-		Markdown.parse("""
-		!!! danger "Initial state unsafe"
-			Initial state: $(states[first_unsafe])
-		""")
+	
+		if first_unsafe == 1
+			Markdown.parse("""
+			!!! danger "Initial state unsafe"
+				Initial state: $(states[first_unsafe])
+			""")
+		else
+			Markdown.parse("""
+			!!! danger "Unsafe trace found"
+				Shielded random agent was found to be unsafe.
+		
+				Out of $traces′ traces, $safety_violations′ were found to be unsafe.
+		
+				First unsafe sate reached: 
+				
+					$(states[first_unsafe]) at index $first_unsafe, time  $(times[first_unsafe])
+				
+				The state before that: 
+				
+					$(states[first_unsafe - 1])
+			""")
+		end
 	else
 		Markdown.parse("""
-		!!! danger "Unsafe trace found"
-			Shielded random agent was found to be unsafe.
-	
-			Out of $traces′ traces, $safety_violations′ were found to be unsafe.
-	
-			First unsafe sate reached: 
-			
-				$(states[first_unsafe]) at time  $(times[first_unsafe])
-			
-			The state before that: 
-			
-				$(states[first_unsafe - 1])
+		!!! success "✅ All traces safe"
+			$safety_violations′ out of the $traces′ traces were unsafe.
 		""")
 	end
-end end
+end
+
+# ╔═╡ 82c1573c-1b09-4f22-b125-4a0d3b7c9b13
+π((-4.0, -5.0, 9.75))
 
 # ╔═╡ f1347d14-e9c8-469e-bc93-7d10724bb49b
-@bind t NumberField(0:(isnothing(example_of_unsafe_trace′) ? 120 : example_of_unsafe_trace′.times[end]))
+@bind trace_index NumberField(1:length(example_trace′.states))
 
 # ╔═╡ 1537138d-1e9a-4c2e-a1ce-0e3b696d5c8d
 md"""
@@ -496,11 +574,13 @@ md"""
 
 # ╔═╡ 05305c4a-a1e6-40b4-bb94-e15e77929ef3
 begin
-	function draw′(grid)
-		draw′(grid, v_ego, v_front, distance)
+	function draw′(grid; slice=nothing)
+		draw′(grid, v_ego, v_front, distance; slice)
 	end
-	function draw′(grid, v_ego, v_front, distance)
-		slice = [box(grid, v_ego, v_front, distance).indices[1], :, :]
+	function draw′(grid, v_ego, v_front, distance; slice=nothing)
+		if isnothing(slice)
+			slice = [box(grid, v_ego, v_front, distance).indices[1], :, :]
+		end
 		colors = [v for (k, v) in sort(action_color_dict)]
 		labels = [k for (k, v) in sort(action_color_dict)]
 		labels = [int_to_actions(CCAction, l) for l in labels]
@@ -526,40 +606,34 @@ partition = box(shield, v_ego, v_front, distance)
 # ╔═╡ 0018900a-03ed-437f-a4ce-b1e967269ac3
 get_value(partition)
 
-# ╔═╡ b59bea05-7e76-4c0c-ab9d-6fbde1269b35
-π((v_ego, v_front, distance))
-
 # ╔═╡ 99aabe32-7c65-4a9d-9397-ba2db2ca5cab
 @bind action Select([backwards, neutral, forwards])
 
 # ╔═╡ 5b3b198d-f0df-4991-8eb7-f208418b0be0
 possible_outcomes(model, partition, action)
 
-# ╔═╡ d20f3aba-e13d-4ff6-9d4d-739945311f19
-simulate_point(m, (v_ego, v_front, distance), 0.5, action)
-
-# ╔═╡ d6c26dbe-f435-4242-9fe4-13cedadea1b0
-simulate_point(m′, (v_ego, v_front, distance), 0.5, action)
-
 # ╔═╡ fa369c7d-ba1b-4aa0-bd83-2e2d4b8486f4
 @bind show_point CheckBox(default=true)
 
 # ╔═╡ ac4c993e-0f18-4829-8dd1-450b79c399b8
-s_t = let
-	if !isnothing(example_of_unsafe_trace′)
-		i = indexof((==(t)), example_of_unsafe_trace′.times)[1]
-		example_of_unsafe_trace′.states[i]
-	end
-end
+s_t = example_trace′.states[trace_index]
+
+# ╔═╡ 628d471f-ccd7-464d-a7b1-7174712d6b86
+[simulate_point(m′, s_t, r, a) 
+	for a in instances(CCAction)
+	for r in 0:0.5:1]
+
+# ╔═╡ c5de5e40-571d-4385-86ac-3f30f9aa6566
+example_trace′.times[trace_index]
 
 # ╔═╡ 28f000ec-9538-4c9c-afbc-ece4af32d3af
 	let
-	draw′(shield)
+	partition = box(shield, π(s_t))
+	slice = [partition.indices[1], :, :]
+	draw′(shield; slice)
 	plot!(margin=3mm)
 	
-	if show_point && !isnothing(example_of_unsafe_trace′)
-		partition = box(shield, π(s_t...))
-		slice = [partition.indices[1], :, :]
+	if show_point
 		draw_barbaric_transition!(model, partition, action, slice)
 	else
 		plot!()
@@ -567,8 +641,12 @@ end
 end
 
 # ╔═╡ Cell order:
-# ╟─1e159603-fc61-45f8-9595-f75e55318344
 # ╠═c1bdc9f0-3d96-11ee-00af-b341a715281c
+# ╟─da8a843d-b5c7-4155-b90c-3df160996c13
+# ╠═af869f6a-d132-4c75-b0b4-440f76352c70
+# ╠═75dbc088-74a1-4a7a-ae59-f2163b99b6f1
+# ╠═ea83ab65-934a-4734-b562-f9f5223fb34f
+# ╟─1e159603-fc61-45f8-9595-f75e55318344
 # ╠═d2204fe6-a71e-4131-a568-349572ce28d4
 # ╠═3a57c06f-0adb-4f92-9f64-f22edbefcadf
 # ╟─5f3af2ba-af4e-4591-bc56-dbebfcb06de5
@@ -576,7 +654,6 @@ end
 # ╠═7ba18477-7d3a-4004-b422-46e7c850fc23
 # ╠═7dd403fc-878d-45d3-9976-655f10dfd8bc
 # ╠═0fedc544-3a81-45b3-b8b0-94c86d291f1b
-# ╠═6134ef59-6377-466b-952d-bee90e421b80
 # ╠═f8a8834e-b8fe-4dc8-8528-4b72148fda6f
 # ╠═0fba5442-bba5-4a82-9821-77068368227e
 # ╠═1431a6cc-1a91-4357-b624-8ed77311a426
@@ -596,36 +673,42 @@ end
 # ╠═25e8dc25-5570-4b39-b353-2df981cc8c9a
 # ╠═e400c9d4-4132-431c-aa99-b551b09fbccd
 # ╠═bd4395db-3c26-4235-9a42-fa6a7eca7041
-# ╟─da8a843d-b5c7-4155-b90c-3df160996c13
-# ╠═af869f6a-d132-4c75-b0b4-440f76352c70
-# ╠═75dbc088-74a1-4a7a-ae59-f2163b99b6f1
-# ╠═ea83ab65-934a-4734-b562-f9f5223fb34f
 # ╟─2418bf90-b0ec-4cb9-b3fd-bf2b91d2ff33
 # ╠═b7d66268-8a40-499d-aaca-d6e59f0ee14f
 # ╠═0018900a-03ed-437f-a4ce-b1e967269ac3
 # ╠═5b3b198d-f0df-4991-8eb7-f208418b0be0
 # ╟─0382588a-ac96-4528-9fee-67ab93d4a1f8
+# ╠═baa9e7b6-9e93-4fe2-bf53-4c37803f3820
 # ╠═107b960f-1a75-41f9-9cb9-195877ad6184
 # ╠═c971bbe4-bc6b-49dd-940d-3277017e99bc
 # ╠═93741008-85f5-479c-908e-27a3716ef25f
-# ╟─db29fb3a-0e95-44f6-b324-5238ac02427c
+# ╠═db29fb3a-0e95-44f6-b324-5238ac02427c
 # ╟─b4eea529-3c88-4e9d-b1b1-99fe2f9c4f94
 # ╟─fb88f6f9-cfba-4ccd-ba56-9e84a56ce63c
+# ╠═0518e3ff-8c3f-49b4-92ba-871b22c7adc7
+# ╟─6ed54088-757e-4906-ab95-769563050ef9
+# ╠═de7136f9-32ed-4042-b649-1bdfb9624685
 # ╠═3af3824c-c032-46b6-b6ae-db9f01691183
-# ╠═2b1c93bc-d6f1-4591-8fde-c32a78138325
+# ╠═4c6fac5d-86b9-47d0-a654-77e3adc679f4
+# ╠═a9b3ccc3-58f6-4b65-9923-b4c213df6752
+# ╠═fb24a4e7-7754-4a35-bbfe-99dcf59ba50b
+# ╠═6134ef59-6377-466b-952d-bee90e421b80
 # ╠═38d987a3-eeb8-45fc-a86f-53e5337663c2
-# ╠═b59bea05-7e76-4c0c-ab9d-6fbde1269b35
+# ╠═7854e7a3-0749-45b9-85b9-ab43bcca10d1
+# ╠═223fc1d3-6b1c-4807-bc74-7bf4a63ed859
 # ╠═9d64b733-6a09-4997-b363-04281f52fb26
-# ╟─38e1e427-6b1f-402c-93d5-8847e2563c32
 # ╠═aea60129-59db-4597-9bb5-715b83560dd0
+# ╠═71155dd3-0abc-4119-9e31-885663a32cec
 # ╠═ddd02244-044f-480c-833a-bd895f0adb79
+# ╠═ed2811a6-c557-4f08-855c-59425a0d5c72
 # ╠═0dd174f4-ca16-4ad3-90e7-783da9f59366
 # ╟─5aaa1518-652c-4980-9899-0d7ae60008ea
+# ╠═82c1573c-1b09-4f22-b125-4a0d3b7c9b13
 # ╠═f1347d14-e9c8-469e-bc93-7d10724bb49b
 # ╟─1537138d-1e9a-4c2e-a1ce-0e3b696d5c8d
 # ╠═99aabe32-7c65-4a9d-9397-ba2db2ca5cab
-# ╠═d20f3aba-e13d-4ff6-9d4d-739945311f19
-# ╠═d6c26dbe-f435-4242-9fe4-13cedadea1b0
+# ╠═628d471f-ccd7-464d-a7b1-7174712d6b86
 # ╠═fa369c7d-ba1b-4aa0-bd83-2e2d4b8486f4
 # ╠═ac4c993e-0f18-4829-8dd1-450b79c399b8
+# ╠═c5de5e40-571d-4385-86ac-3f30f9aa6566
 # ╠═28f000ec-9538-4c9c-afbc-ece4af32d3af
