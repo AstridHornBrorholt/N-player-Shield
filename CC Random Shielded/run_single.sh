78d094f18dead38545ce28e198a9cfa302049c4a
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-01-25 11:06:15 +0100

    Try fixing the Run Slurm script in Random Shielded

--- CC Random Shielded/run_single.sh
+++ CC Random Shielded/run_single.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 verifyta=${verifyta:-"$HOME/opt/uppaal-5.1.0-beta5-linux64/bin/verifyta.sh"}
+RANDOM_FLEET=${RANDOM_FLEET:-"$PWD/Random Fleet.xml"}
 RESULTS="$HOME/Results/CC Random Shielded"
 echo "Running single experiment."
+# Create results dir if not exists
 [ -d "$RESULTS" ] || mkdir "$RESULTS"
 
 # Check if the first argument is "Safety"
 if [ "${1}" = "Safety" ]; then
-    $verifyta -s "Random Fleet.xml" "Safety.q" &>> "$RESULTS/Safety.txt"
+    $verifyta -s "$RANDOM_FLEET" "Safety.q" &>> "$RESULTS/Safety.txt"
     
 # Check if the first argument is a number
 elif [[ "${1}" =~ ^[0-9]+$ ]]; then
-    $verifyta -s "Random Fleet.xml" "D${1}.q" &>> "$RESULTS/D${1}.txt"
+    $verifyta -s "$RANDOM_FLEET" "D${1}.q" &>> "$RESULTS/D${1}.txt"
     
 else
     echo "The first argument is neither 'Safety' nor a number"
--- CC Random Shielded/run_slurm.sh
+++ CC Random Shielded/run_slurm.sh
@@ -8,12 +8,18 @@ export log_output="$RESULTS/log.txt"
 date >> "$log_output"
 ARGS="--out=/dev/null --exclude=rome0[1-3],dhabi0[1-3],naples0[1-3],vmware0[1-4] --partition=cpu -n1 --mem=16G --job-name StdCC"
 
+# Make a copy of the thing. The uppaal file.
+export RANDOM_FLEET="$RESULTS/Random Fleet.xml"
+# Put in correct shield.
+export SHIELD="$RESULTS/libshield.so"
+cp "../CC Shield/libshield.so" "$SHIELD"
+sed "s#/home/asger/Documents/Files/Arbejde/AAU/Artikler/N-player Shield/CC Shield/libshield.so#$SHIELD#" "Random Fleet.xml" > "$RANDOM_FLEET"
+
+# Schedule slurm jobs
 for ((i=0; i<=19; i++))
 do
     sbatch $ARGS ./run_single.sh $i
     echo "Job scheduled."
 done
-
-
 sbatch $ARGS ./run_single.sh Safety
 echo "Job scheduled."
