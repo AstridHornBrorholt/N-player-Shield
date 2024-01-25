76a57ed7e54b280fb7d7a0d48d1c2db77b3ed7ea
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-01-25 11:43:51 +0100

    zip and download results

--- download_results.sh
+++ download_results.sh
@@ -3,14 +3,17 @@ scp -rd 'deismcc:/nfs/home/cs.aau.dk/oq82yk/Results/N-player\ CP.zip' "$(pwd)" >
 scp -rd 'deismcc:/nfs/home/cs.aau.dk/oq82yk/Results/N-player\ CC\ Centralized\ Controller.zip' "$(pwd)" > /dev/null
 scp -rd 'deismcc:/nfs/home/cs.aau.dk/oq82yk/Results/N-player\ CP\ Centralized\ Controller.zip' "$(pwd)" > /dev/null
 scp -rd 'deismcc:/nfs/home/cs.aau.dk/oq82yk/Results/N-player\ CC\ Centralized\ Shield.zip' "$(pwd)" > /dev/null
+scp -rd 'deismcc:/nfs/home/cs.aau.dk/oq82yk/Results/N-player\ CC\ Random\ Shielded.zip' "$(pwd)" > /dev/null
 unzip -qo "./N-player CC.zip" -d "$HOME/Results/"
 unzip -qo "./N-player CP.zip" -d "$HOME/Results/"
 unzip -qo "./N-player CC Centralized Controller.zip" -d "$HOME/Results/"
 unzip -qo "./N-player CP Centralized Controller.zip" -d "$HOME/Results/"
 unzip -qo "./N-player CC Centralized Shield.zip" -d "$HOME/Results/"
+unzip -qo "./N-player CC Random Shielded.zip" -d "$HOME/Results/"
 rm "./N-player CC.zip"
 rm "./N-player CP.zip"
 rm "./N-player CC Centralized Controller.zip"
 rm "./N-player CP Centralized Controller.zip"
 rm "./N-player CC Centralized Shield.zip"
+rm "./N-player CC Random Shielded.zip"
 sed -i "s>/nfs/home/cs.aau.dk/.*yk>/home/asger>g" $HOME/Results/N-player*/*Runs/Repetition*/Models/*
--- zip_results.sh
+++ zip_results.sh
@@ -5,9 +5,11 @@ rm "N-player CP.zip"
 rm "N-player CC Centralized Shield.zip"
 rm "N-player CC Centralized Controller.zip"
 rm "N-player CP Centralized Controller.zip"
+rm "N-player CC Random Shielded.zip"
 zip -r "N-player CC.zip" "N-player CC"/*
 zip -r "N-player CP.zip" "N-player CP"/*
 zip -r "N-player CC Centralized Shield.zip" "N-player CC Centralized Shield"/*
 zip -r "N-player CC Centralized Controller.zip" "N-player CC Centralized Controller"/*
 zip -r "N-player CP Centralized Controller.zip" "N-player CP Centralized Controller"/*
+zip -r "N-player CC Random Shielded.zip" "N-player CC Random Shielded"/*
 cd $previous_working_dir
