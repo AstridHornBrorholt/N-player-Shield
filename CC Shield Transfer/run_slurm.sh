c6ad1f444ff994bf3df420d42507281a9d05c050
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-02-06 13:17:47 +0100

    Sanity check for the CC Shield Transfer experiment

--- CC Shield Transfer/run_slurm.sh
+++ CC Shield Transfer/run_slurm.sh
@@ -7,28 +7,18 @@ date >> "$log_output"
 ARGS="--out=/dev/null --exclude=rome0[1-3],dhabi0[1-3],naples0[1-3],vmware0[1-4] --partition=cpu -n1 --mem=16G --job-name StdCC"
 
 export max_cars=10
-repetitions=10
+repetitions=5
 
 for ((r=1; r<=$repetitions; r++))
 do
     export repetition=$r
     
-    export runs=2500
+    export runs=2502
     export checks=1000
     sbatch $ARGS ./run_single.sh
     echo "Job scheduled."
     
-    export runs=5000
-    export checks=1000
-    sbatch $ARGS ./run_single.sh
-    echo "Job scheduled."
-    
-    export runs=10000
-    export checks=1000
-    sbatch $ARGS ./run_single.sh
-    echo "Job scheduled."
-    
-    export runs=20000
+    export runs=5002
     export checks=1000
     sbatch $ARGS ./run_single.sh
     echo "Job scheduled."
