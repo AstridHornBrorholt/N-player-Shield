b076d26b952289104e06b829b47cd692c6728da0
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-02-19 17:01:06 +0100

    Augh

--- CC Centralized Controller/run_slurm.sh
+++ CC Centralized Controller/run_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Scheduling slurm jobs"
 
-export log_output="$HOME/Results/N-player CC Centralized Learning/log.txt"
+export log_output="$HOME/Results/N-player CC Centralized Controller/log.txt"
 
 date >> "$log_output"
 ARGS="--out=/dev/null --exclude=rome0[1-3],dhabi0[1-3],naples0[1-3],vmware0[1-4] --partition=cpu -n1 --mem=16G --job-name 'CCCentralizedController'"
