63000a74e47133cfc5a5b8e5da657b2e3a18ed89
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-02-22 10:50:30 +0100

    CC Non-specialized

---
+++ CC Non-specialized/Run Experiment.jl
@@ -0,0 +1,121 @@
+## Preface ##
+using Dates
+# The fruit is there to distinguish different runs writing to the same output concurrently. This doesn't seem to be a problem after all but I enjoy the splash of colour.
+emoji = ["🔴", "🟠", "🟡", "🟢", "🔵", "🟣", "🟤", "⚫", "⚪"]
+🔴🟢 = join(rand(emoji, 2), "")
+node = get(ENV, "SLURMD_NODENAME", "local")
+function status(str) 
+    time = Dates.format(Dates.now(), "dd/mm HH:MM")
+    println("$time $node $🔴🟢 $str")
+    flush(stdout)
+end
+using Pkg
+Pkg.activate("..")
+
+using ArgParse
+include("../CC/Create Fleet.jl")
+
+## Args and Constants ##
+⨝ = joinpath
+← = push!
+
+begin
+	s = ArgParseSettings()
+	@add_arg_table s begin
+	     "--runs"
+			arg_type=Int
+			required=true
+		"--checks"
+			arg_type=Int
+			required=true
+        "--total-cars"
+            arg_type=Int
+            required=true
+        "--repetition"
+            arg_type=Int
+            required=true
+		"--results-dir"
+			default=homedir() ⨝ "Results/N-player CC Non-specialized"
+        "--verifyta-path"
+            default=homedir() ⨝ "opt/uppaal-5.0.0-linux64/bin/verifyta.sh"
+        "--blueprint-path"  
+            default=pwd() ⨝ "../CC/Fleet_blueprint.xml"
+        "--shield-path"  
+            default=pwd() ⨝ "../CC Shield/libshield.so"
+        "--skip-training"
+            action=:store_true
+            help="MISNOMER: Use car1.json for all cars in the fleets (must exist) without training any strategies."
+	end
+end;
+
+args = parse_args(s)
+
+runs = args["runs"]
+checks = args["checks"]
+total_cars = args["total-cars"]
+repetition = args["repetition"]
+results_dir = args["results-dir"]
+verifyta_path = args["verifyta-path"]
+skip_training = args["skip-training"]
+status("Starting... $((;runs, checks, total_cars, repetition))")
+
+isdir(results_dir) || mkdir(results_dir) # Error if path is invalid except if it is only the last folder missing.
+isfile(verifyta_path) || error("File verifyta not found at path $verifyta_path")
+
+verifyta_args = "-s --epsilon 0.001 --max-iterations 1 --good-runs $runs --total-runs $runs --runs-pr-state $runs"
+
+verifyta_call = String[
+    verifyta_path,
+    split(verifyta_args, " ")...
+]
+
+blueprint_path = args["blueprint-path"]
+shield_path = args["shield-path"]
+
+## Resolving Paths ##
+isfile(shield_path) || error("Shield file not found at $shield_path")
+
+shield_path′ = results_dir ⨝ basename(shield_path)
+cp(shield_path, shield_path′, force=true)
+shield_path = shield_path′
+
+working_dir = results_dir ⨝ "$runs Runs" ⨝ "Repetition $repetition"
+mkpath(working_dir)
+query_results_dir = working_dir ⨝ "Query Results"
+mkpath(query_results_dir)
+models_dir = working_dir ⨝ "Models"
+mkpath(models_dir)
+
+struct ExceptionWithNodeID <: Exception
+    captured_exception
+    node_id
+end
+
+# Run a fleet with one learner car and one random car in front,
+#plus as many cars in betweeen, as there are strategies provided in `strategy_paths`
+function do_run(strategy_paths)
+    N = length(strategy_paths) + 2
+    status("Running Fleet of $N Cars...  (repetition=$repetition)")
+    outfile = query_results_dir ⨝ "Fleet of $N Cars.txt"
+    model_path, queries_path = create_fleet(blueprint_path, strategy_paths, shield_path, models_dir; checks, skip_training)
+    open(outfile, "w") do io
+        result = [verifyta_call..., model_path, queries_path] |> Cmd |> read |> String
+        write(io, result)
+    end
+    status("Done running Fleet of $N Cars.  (repetition=$repetition)")
+end
+
+## Mainmatter ##
+try
+    # Train a distributed strategy on the first car
+    do_run([])
+    strategy_path = (working_dir ⨝ "Models/car1.json")
+    strategy_paths = [strategy_path for _ in 2:total_cars - 1]
+
+    # Do the actual run
+    do_run(strategy_paths)
+
+    status("All done.")
+catch e
+    throw(ExceptionWithNodeID(e, node))
+end
---
+++ CC Non-specialized/run_single_cli.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export runs=${1}
+export checks=${2}
+export total_cars=${3}
+export repetition=1
+./run_single.sh
