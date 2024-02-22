3c0b9b8df619579f9b954b5e2a3b30dabbb450ac
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-02-22 09:02:06 +0100

    Misc

--- CC Centralized Controller/run_single_cli.sh
+++ CC Centralized Controller/run_single_cli.sh
@@ -3,10 +3,5 @@ export runs=${1}
 export checks=${2}
 export max_cars=${3}
 export repetition=1
-
-
-for ((f=2; f<=$max_cars; f++))
-do
-    export fleet_size=$f
-    ./run_single.sh
-done
+export fleet_size=10
+./run_single.sh
