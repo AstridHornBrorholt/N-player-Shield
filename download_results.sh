95a01e986edf7e595a73c547f44b10e1d292242a
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-07-09 10:23:17 +0200

    Also download from MCC3

--- download_results.sh
+++ download_results.sh
@@ -1,4 +1,9 @@
 scp -rd 'deismcc:/nfs/home/cs.aau.dk/oq82yk/Results/N-player.zip' "$(pwd)" > /dev/null
 unzip -qo "./N-player.zip" -d "$HOME/Results/"
 rm "./N-player.zip"
+
+scp -rd 'mcc3:/nfs/home/cs.aau.dk/oq82yk/Results/N-player.zip' "$(pwd)" > /dev/null
+unzip -qo "./N-player.zip" -d "$HOME/Results/"
+rm "./N-player.zip"
+
 ./update_filepaths.sh
