820791061b3e691adfed00061effe76000e7a192
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2024-08-13 10:22:42 +0200

    Choose between mcc3 and mcc2 cluster

--- download_results.sh
+++ download_results.sh
@@ -1,9 +1,17 @@
-scp -rd 'deismcc:/nfs/home/cs.aau.dk/oq82yk/Results/N-player.zip' "$(pwd)" > /dev/null
-unzip -qo "./N-player.zip" -d "$HOME/Results/"
-rm "./N-player.zip"
+#!/bin/bash
 
-scp -rd 'mcc3:/nfs/home/cs.aau.dk/oq82yk/Results/N-player.zip' "$(pwd)" > /dev/null
-unzip -qo "./N-player.zip" -d "$HOME/Results/"
-rm "./N-player.zip"
+arg1=${1:-"mcc3"}
+
+if [[ "$arg1" == "mcc2" ]]; then
+    echo "Downlaoding results from mcc2..."
+    scp -rd 'deismcc:/nfs/home/cs.aau.dk/oq82yk/Results/N-player.zip' "$(pwd)" > /dev/null
+    unzip -qo "./N-player.zip" -d "$HOME/Results/"
+    rm "./N-player.zip"
+else 
+    echo "Downlaoding results from mcc3..."
+    scp -rd 'mcc3:/nfs/home/cs.aau.dk/oq82yk/Results/N-player.zip' "$(pwd)" > /dev/null
+    unzip -qo "./N-player.zip" -d "$HOME/Results/"
+    rm "./N-player.zip"
+fi
 
 ./update_filepaths.sh
